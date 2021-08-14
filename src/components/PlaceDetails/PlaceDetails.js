31f57f6f4b25ff59b4ac23dfab1b96c9d6f14537
ysonbrian <znlsakxls@gmail.com>
2021-08-14 22:49:51 +0900

    Fixed bugs

--- src/components/PlaceDetails/PlaceDetails.js
+++ src/components/PlaceDetails/PlaceDetails.js
@@ -1,7 +1,12 @@
 import React from 'react';
 
 const PlaceDetails = ({ place }) => {
-  return <h1>{place.name}</h1>;
+  return (
+    <div>
+      <h1>{place.name}</h1>
+      <h2>Hello</h2>
+    </div>
+  );
 };
 
 export default PlaceDetails;
