7a4f64ce9df693cc87336751cdb0898b2b10f355
ysonbrian <znlsakxls@gmail.com>
2021-08-15 20:19:34 +0900

    Working with APIs

--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -6,10 +6,10 @@ import Rating from '@material-ui/lab';
 
 import useStyles from './styles';
 
-const Map = () => {
+const Map = ({ setCoordinates, setBounds, coordinates }) => {
   const classes = useStyles();
   const isMobile = useMediaQuery('(min-width:600px)');
-  const coordinates = { lat: 0, lng: 0 };
+
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
@@ -19,8 +19,12 @@ const Map = () => {
         defaultZoom={14}
         margin={[50, 50, 50, 50]}
         options={''}
-        onChange={''}
-        onChildClick={''}
+        onChange={(e) => {
+          console.log(e);
+          setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+          setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
+        }}
+        // onChildClick={''}
       ></GoogleMapReact>
     </div>
   );
