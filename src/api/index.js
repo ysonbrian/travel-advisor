1732459cd42d4961e11e9c46efab491e494eb408
ysonbrian <znlsakxls@gmail.com>
2021-08-23 23:44:09 +0900

    Scroll Places && Filters

--- src/api/index.js
+++ src/api/index.js
@@ -1,24 +1,25 @@
 import axios from 'axios';
 
-const URL =
-  'https://travel-advisor.p.rapidapi.com/restaurants/list-in-boundary';
-
-export const getPlacesData = async (sw, ne) => {
+export const getPlacesData = async (type, sw, ne) => {
   try {
     const {
       data: { data },
-    } = await axios.get(URL, {
-      params: {
-        bl_latitude: sw.lat,
-        tr_latitude: ne.lat,
-        bl_longitude: sw.lng,
-        tr_longitude: ne.lng,
-      },
-      headers: {
-        'x-rapidapi-key': '54608a6679msh9648ecd75cd984fp1f3fcdjsn8a2a20dd832b',
-        'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
-      },
-    });
+    } = await axios.get(
+      `https://travel-advisor.p.rapidapi.com/${type}/list-in-boundary`,
+      {
+        params: {
+          bl_latitude: sw.lat,
+          tr_latitude: ne.lat,
+          bl_longitude: sw.lng,
+          tr_longitude: ne.lng,
+        },
+        headers: {
+          'x-rapidapi-key':
+            '54608a6679msh9648ecd75cd984fp1f3fcdjsn8a2a20dd832b',
+          'x-rapidapi-host': 'travel-advisor.p.rapidapi.com',
+        },
+      }
+    );
     console.log(data);
     return data;
   } catch (error) {
--- src/components/Map/Map.js
+++ src/components/Map/Map.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { useState } from 'react';
 import GoogleMapReact from 'google-map-react';
 import { Paper, Typography, useMediaQuery } from '@material-ui/core';
 import LocationOnOutlinedIcon from '@material-ui/icons/LocationOnOutlined';
@@ -6,10 +6,15 @@ import Rating from '@material-ui/lab/Rating';
 
 import useStyles from './styles';
 
-const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
+const Map = ({
+  setCoordinates,
+  setBounds,
+  coordinates,
+  places,
+  setChildClicked,
+}) => {
   const classes = useStyles();
   const isDesktop = useMediaQuery('(min-width:600px)');
-
   return (
     <div className={classes.mapContainer}>
       <GoogleMapReact
@@ -24,7 +29,9 @@ const Map = ({ setCoordinates, setBounds, coordinates, places }) => {
           setCoordinates({ lat: e.center.lat, lng: e.center.lng });
           setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
         }}
-        // onChildClick={''}
+        onChildClick={(child) => {
+          setChildClicked(child);
+        }}
       >
         {places?.map((place, i) => (
           <div
